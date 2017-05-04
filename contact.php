ee103ce145442c95e56ac2885ddb8ec9dd12b7b7
Jonatan Johnsson <bighuntcinema@gmail.com>
2017-05-04 13:12:25 +0300

    Fixed projects page, replaced more dead links

--- contact.php
+++ contact.php
@@ -127,6 +127,6 @@ if (isset($_POST['submit'])) { // Här kollar vi om "Skicka"-knappen är klickad
 <!-- jQuery first, then Tether, then Bootstrap JS. -->
 <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+<script src="js/bootstrap.min.js"></script>
   </body>
 </html>
